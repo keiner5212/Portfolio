f6c0f33a686e4576961ea214f2458a8fdd695526
Keiner Alvarado <kjgameplay0000@gmail.com>
2025-08-15 17:26:17 -0500

    feat: nextjs optimizations

--- next.config.js
+++ next.config.js
@@ -1,12 +1,43 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+const WebpackObfuscatorPlugin = require("webpack-obfuscator");
+const CompressionPlugin = require("compression-webpack-plugin");
+
+module.exports = {
   images: {
-    unoptimized: true,
+    formats: ["image/avif", "image/webp"],
+  },
+
+  webpack(config, { isServer, dev }) {
+    //obfuscate
+    if (!isServer && !dev) {
+      config.plugins.push(
+        new WebpackObfuscatorPlugin(
+          {
+            rotateStringArray: true,
+            stringArray: true,
+            stringArrayEncoding: ["base64"],
+            stringArrayThreshold: 0.75,
+          },
+          [],
+        ),
+      );
+
+      //gzip
+      config.plugins.push(
+        new CompressionPlugin({
+          algorithm: "gzip",
+          test: /\.(js|css|html|svg)$/,
+        }),
+        new CompressionPlugin({
+          filename: "[path][base].br",
+          algorithm: "brotliCompress",
+          test: /\.(js|css|html|svg)$/,
+          compressionOptions: { level: 11 },
+          threshold: 10240,
+          minRatio: 0.8,
+        }),
+      );
+    }
+
+    return config;
   },
 };
-
-module.exports = nextConfig;
